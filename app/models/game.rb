c193f222b6769585647980ba307e37dd8625f8c8
Jeni Dang <jenipdang@yahoo.com>
2022-02-28 16:11:46 -0800

    commit tree 703d9ce635801afbd6cd789cc70e933923b58cfa

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,14 @@
 class Game < ActiveRecord::Base
-  
+
+    # def reviews 
+    #     Review.where(game_id: self.id)
+    # end
+
+    has_many :reviews
 end
+
+#SQL that AR generates for this query:
+
+# SELECT "reviews". *
+# FROM "reviews"
+# WHERE "reviews"."games_id" = 1
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,20 @@
 class Review < ActiveRecord::Base
+    # # a review belongs to a game
+    # def game 
+    #     #self is the review instance
+    #     Game.find(self.game_id)
+    # end
+
+    #custom built macros with AR. Give same result as custom instance method above
+    #belongs_to is a method that is inherited from ActiveRecord::Base that takes an argument of a symbol
+    #name of the symbole we are passing into belongs_to MUST be singular
+    belongs_to :game
   
 end
+
+#SQL that AR generated from the query:
+
+# SELECT "games".*
+# FROM "games"
+# WHERE "games"."id" = 1
+# LIMIT = 1
